03c72eeaf2fd2f1b852f5fddef5a11d23a36caee
Hunter Kam <36145201+Hone648@users.noreply.github.com>
2023-09-27 18:42:47 -0600

    Added input voltage logic to monitor when the alternator is running

--- Van.ino
+++ Van.ino
@@ -3,8 +3,9 @@
 #include <Adafruit_Sensor.h>
 
 PCF8574 relayBoard(0x27);
+//Relay 1
 uint8_t lithiumVout = P7;
-uint8_t leadAcidVout = P6;
+uint8_t relay2 = P6;
 uint8_t relay3 = P5;
 uint8_t relay4 = P4;
 uint8_t relay5 = P3;
@@ -17,28 +18,38 @@ void setup() {
   Serial.begin(115200);
   pinMode(A6, INPUT);
   relayBoard.pinMode(lithiumVout, OUTPUT);
-  relayBoard.pinMode(leadAcidVout, OUTPUT);
+  relayBoard.pinMode(relay2, OUTPUT);
   relayBoard.pinMode(relay3, OUTPUT);
   relayBoard.pinMode(relay4, OUTPUT);
   relayBoard.pinMode(relay5, OUTPUT);
   relayBoard.pinMode(relay6, OUTPUT);
   relayBoard.pinMode(relay7, OUTPUT);
   relayBoard.pinMode(relay8, OUTPUT);
-  Serial.print("Initializing the Relay Board."); delay(500);
+  Serial.println("Initializing the Relay Board..."); delay(500);
 	if (relayBoard.begin()){
-		Serial.print("."); delay(500);
-    Serial.println(". ...and I lava you!"); delay(500);
+		Serial.print("Connected to Address: "); Serial.println("0x27");
 	}else{
 		Serial.println("Unable to connect to relay board. Check address and connections."); delay(500);
 	}
 }
 
-
 void loop() {
-  bool alternator = true;
-  
-  relayBoard.digitalWrite(lithiumVout, HIGH);
-  delay(2000);
+  static int counter = 0;
+  int batteryVoltage = analogRead(A0);
+  bool alternator = false;
+  if(batteryVoltage < 842) {
+    alternator = false;
+  } else if (batteryVoltage > 842) {
+    alternator = true;
+  }
+  Serial.println(alternator);
+  if(alternator) {
+    relayBoard.digitalWrite(lithiumVout, HIGH);
+  } else {
+    relayBoard.digitalWrite(lithiumVout, LOW);
+  }
+  Serial.println(batteryVoltage);
+  delay(13000);
 }
 
 
